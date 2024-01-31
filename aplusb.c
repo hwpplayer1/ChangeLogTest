6b59991279b9c67a5de746702813f67244f339c4
Mert Gör <mertgor@masscollabs.xyz>
2024-02-01 02:26:01 +0300

    ChangeLog c files and a test text file

---
+++ aplusb.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  int a = 1;
+  int b = 2;
+  c = a + b;
+  printf("a + b = c and it is : %d\n", c);
+  return 0;
+}
---
+++ axb.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  int a = 10;
+  int b = 20;
+  c = a * b;
+  printf("a x b = c and it is = %d\n", c);
+  return 0;
+}
